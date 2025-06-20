53b3cce69957e15975c810449e3abfa8a7de9801
Johnny <Johnny@WIN-79R243S9KUR>
2025-06-20 00:41:37 -0700

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿namespace MauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnSendMessageClicked(object? sender, EventArgs e)
+        {
+        }
+
+        private async Task LoadCaptcha() {
+            try
+            {
+                string captchaApiUrl = null;
+            }
+    }
+}
